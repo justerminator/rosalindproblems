c7c7dfff1c57dc8db8732322c8d806c0a8a012af
Justin Lau <justinjlau@yahoo.com>
2023-08-02 20:20:25 -0700

    8/2 calc protein mass

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import urllib3 # get protein fasta from uniprot
 from Bio import SeqIO, Seq # fasta parsing and manipulation   
 
 
-f = open("rosalind_mrna.txt", 'r')
+f = open("rosalind_prtm.txt", 'r')
 
 # 7/23/23
 
@@ -468,6 +468,21 @@ def possiblemRNA():
 
     return possibilityAmnt
 
-print(possiblemRNA())
+# 8/2/23
+
+# Calculating Protein Mass
+
+monoiso_mass_table = { "A" : 71.03711, "C" : 103.00919, "D" : 115.02694, "E" : 129.04259, "F" : 147.06841, "G" : 57.02146, "H" : 137.05891, "I" : 113.08406, "K" : 128.09496, "L" : 113.08406, "M" : 131.04049, "N" : 114.04293, "P" : 97.05276, "Q" : 128.05858, "R" : 156.10111, "S" : 87.03203, "T" : 101.04768, "V" : 99.06841, "W" : 186.07931, "Y" : 163.06333, }
+
+def proteinMass():
+    curr = f.readline().strip()
+
+    result = 0
+    for letter in curr:
+        result += monoiso_mass_table[letter]
+
+    return result
+
+print(proteinMass())
 
 f.close()
